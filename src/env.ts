0bcb46a358e1a86b4ec4e8154289c50c105b93cb
Rahmat Sulistio <rr7465457@gmail.com>
2024-01-08 19:51:09 +0700

    :package: Add @t3-oss/env-core for env validation

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+import { createEnv } from '@t3-oss/env-core'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    PORT: z.coerce.number(),
+  },
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+})
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
+import { env } from './env'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const PORT = env.PORT
+
+const app = new Elysia()
+
+app.get('/', () => 'Hello Elysia')
+
+app.listen(PORT)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+)
